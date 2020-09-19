4b1291c7c9fa759dd2afdb3fd6368f423f1196ba
KTheXIII <24420859+KTheXIII@users.noreply.github.com>
2020-09-20 01:07:48 +0200

    First triangle

--- Mandelbrot/src/main.cpp
+++ Mandelbrot/src/main.cpp
@@ -56,6 +56,41 @@ T map(T x, T in_min, T in_max, T out_min, T out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+static unsigned int compile_shader(unsigned int type,
+                                   const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int results;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &results);
+    if (results == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compiled "
+                  << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment")
+                  << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+
+        return 0;
+    }
+
+    return id;
+};
+
 int main(int argc, char const* argv[]) {
     GLFWwindow* window;
     int width = 800, height = 600;
@@ -63,22 +98,99 @@ int main(int argc, char const* argv[]) {
     if (!glfwInit())
         return -1;
 
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Disable window resize
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     window = glfwCreateWindow(width, height, "Mandelbrot set", NULL, NULL);
+
     if (!window) {
+        std::cout << "\u001b[38;5;1mFailed to create GLFW window\u001b[0m"
+                  << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSetFramebufferSizeCallback(
+        window,
+        framebuffer_size_callback); // register the callback for window reszie
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "\u001b[38;5;1mFailed to initialize GLAD\u001b[0m"
+                  << std::endl;
+        return -1;
+    }
+
+    const std::string vertex_shader_source =
+        "#version 410 core\n"
+        "#extension GL_ARB_separate_shader_objects : enable\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main() {\n"
+        "    gl_Position = position;\n"
+        "\n}";
+
+    const std::string fragment_shader_source =
+        "#version 410 core\n"
+        "#extension GL_ARB_separate_shader_objects : enable\n"
+        "layout(location = 0) out vec4 color;\n"
+        "void main() {\n"
+        "    color = vec4(.1, .86, .99, 1.0);"
+        "\n}";
+
+    unsigned int sp = glCreateProgram();
+    unsigned int vs = compile_shader(GL_VERTEX_SHADER, vertex_shader_source);
+    unsigned int fs =
+        compile_shader(GL_FRAGMENT_SHADER, fragment_shader_source);
+
+    glAttachShader(sp, vs);
+    glAttachShader(sp, fs);
+    glLinkProgram(sp);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    float positions[] = {
+        -0.5f, -0.5f, 0.0f, // Left
+        0.0f,  0.5f,  0.0f, // Top
+        0.5f,  -0.5f, 0.0f  // Right
+    };
+
+    unsigned int vbo, vao;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window)) {
+        // inputs
+        process_input(window);
+
+        // Render
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(sp);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(sp);
 
     glfwTerminate();
 
